59f62909e51319c7c3b8ca9115a1debcd43db35c
Kirill <kitrofimov39@gmail.com>
2023-12-13 10:33:19 +0600

    Add a `distanceToNextOccurenceOfCharInFile` internal function to use in `readLine`

--- src/utils/fileUtils.c
+++ src/utils/fileUtils.c
@@ -3,7 +3,7 @@
 #include "fileUtils.h"
 
 /**
- * @brief A function to read the whole line from a filestream. Also see `getline`
+ * @brief Read the whole line from a filestream. Also see `getline` from GNU standard
  *
  * @param line a pointer to string (`char` array) which was `malloc`'ed for `length` characters. If points to `NULL`, the buffer will be allocated inside the function (don't forget to `free` it!) and the value pointed to by `length` is ignored.
  * @param length a pointer to number of characters the `line` was allocated to hold. The value pointed to is ignored if `line` is `NULL`.
@@ -12,19 +12,8 @@
  */
 int readLine(char** line, size_t* length, FILE* fp)
 {
-    // find the closest \n character
-    // determine the needed length for a line
-    size_t neededLength = 0;
-    long startPos = ftell(fp);
-    char c;
-    while ((c = fgetc(fp)) != EOF)
-    {
-        neededLength++;
-        if (c == '\n')
-            break;
-    }
-    neededLength++;  // one more (for null byte)
-    fseek(fp, startPos, SEEK_SET);
+    size_t neededLength = distanceToNextOccurenceOfCharInFile('\n', fp);
+    neededLength++;  // null byte exists
 
     // reallocate the line buffer if it is not long enough
     if (*line == NULL)
@@ -39,3 +28,26 @@ allocate:
 
     return (fgets(*line, *length, fp) == NULL) ? EOF : 0;
 }
+
+/**
+ * @brief Find the distance from the current position in file to the next occurence of a specific character
+ * May be interpreted as "length of a string buffer to be able to read from current character up to this character WITHOUT null byte (add +1 if you need null byte!)"
+ * @param ch char to look for
+ * @param fp file pointer
+ */
+size_t distanceToNextOccurenceOfCharInFile(char ch, FILE* fp)
+{
+    size_t distance = 0;
+    long startPos = ftell(fp);
+    char c;
+    while ((c = fgetc(fp)) != EOF)
+    {
+        distance++;
+        if (c == ch)
+            break;
+    }
+
+    fseek(fp, startPos, SEEK_SET);
+    return distance;
+}
+
--- src/utils/fileUtils.h
+++ src/utils/fileUtils.h
@@ -2,3 +2,5 @@
 #include <stdio.h>
 
 int readLine(char** line, size_t* length, FILE* fp);
+size_t distanceToNextOccurenceOfCharInFile(char ch, FILE* fp);
+
