5b08fadd8178023829f39d08a7e0695fdd7e1b00
Kirill <kitrofimov39@gmail.com>
2024-03-10 16:40:13 +0500

    Add test geometry shader

--- src/main.c
+++ src/main.c
@@ -23,6 +23,53 @@ void vertexShader(void* shaderProgram, void* pVertex, void* pOutput)
     memcpy(pOutput, pVertex, sp->vertexShader.nBytesPerOutputVertex);
 }
 
+void geometryShader(void* shaderProgram, void* pInput, void* pOutput)
+{
+    ShaderProgram* sp = (ShaderProgram*) shaderProgram;
+
+    VertexAttribute inputPosition = sp->vertexShader.outputAttributes[
+        sp->vertexShader.indexOfOutputPositionAttribute
+    ];
+
+    assert(
+        sp->vertexShader.nBytesPerOutputVertex == \
+        sp->geometryShader.nBytesPerOutputVertex
+    );
+    assert(inputPosition.nItems == 3 && inputPosition.type == TYPE_DOUBLE);
+
+    size_t nBytesPerVertex = sp->vertexShader.nBytesPerOutputVertex;
+    
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        size_t inputVertexOffset = i * nBytesPerVertex;
+        void* pInputVertex = (uint8_t*) pInput + inputVertexOffset;
+
+        void* pOutputVertex[2] = {
+            (uint8_t*) pOutput + i * nBytesPerVertex,
+            (uint8_t*) pOutput + (i+3) * nBytesPerVertex
+        };
+
+        double* oldPosition = (double*) ((uint8_t*) pInputVertex + inputPosition.offsetBytes);
+        double newPosition[3] = {
+            oldPosition[0] + 0.5,
+            oldPosition[1],
+            oldPosition[2]
+        };
+
+        memcpy(pOutputVertex[0], pInputVertex, nBytesPerVertex);
+        memcpy(
+            (uint8_t*) pOutputVertex[1] + inputPosition.offsetBytes,
+            newPosition,
+            inputPosition.nItems * sizeof(double)
+        );
+        memcpy(
+            (uint8_t*) pOutputVertex[1] + sp->geometryShader.outputAttributes[1].offsetBytes,
+            (uint8_t*) pInputVertex + sp->vertexShader.outputAttributes[1].offsetBytes,
+            sp->vertexShader.outputAttributes[1].nItems * sizeof(double)
+        );
+    }
+}
+
 void fragmentShader(void* shaderProgram, void* pInterpolated, Color* color)
 {
     ShaderProgram* sp = (ShaderProgram*) shaderProgram;
@@ -43,9 +90,9 @@ int main(int argc, char** argv)
     constructContext(&context);
     
     Vertex data[3] = {
-        {.position = {-0.75, -0.75,  0.0}, .color = {1., 0., 0.}},
-        {.position = { 0.  ,  0.75,  0.0}, .color = {0., 1., 0.}},
-        {.position = { 0.75, -0.75,  0.0}, .color = {0., 0., 1.}}
+        {.position = {-0.5 , -0.25,  0.0}, .color = {1., 0., 0.}},
+        {.position = {-0.25,  0.25,  0.0}, .color = {0., 1., 0.}},
+        {.position = { 0.  , -0.25,  0.0}, .color = {0., 0., 1.}}
     };
     VertexAttribute attributes[2] = {
         {
@@ -73,7 +120,14 @@ int main(int argc, char** argv)
             .indexOfOutputPositionAttribute = 0
         },
         .geometryShader = {
-            .shader = NULL,
+            .shader = geometryShader,
+            .nBytesPerOutputVertex = sizeof(double) * 6,
+            .nOutputAttributes = 2,
+            .outputAttributes = attributes,
+            .indexOfOutputPositionAttribute = 0,
+            .nOutputVertices = 6,
+            .inputPrimitive = PRIMITIVE_TRIANGLES,
+            .outputPrimitive = PRIMITIVE_TRIANGLES
         },
         .fragmentShader = {
             .shader = fragmentShader
