16c13ab0ac69f20ad57cbb84a7de31396312a7db
Kirill <kitrofimov39@gmail.com>
2024-06-16 22:21:41 +0500

    Add wrappers for libc allocation functions that check return values

---
+++ src/alloc.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void* srpMalloc(unsigned long size);
+void srpFree(void* p);
+void* srpRealloc(void* p, unsigned long size);
+
--- src/defines.h
+++ src/defines.h
@@ -1,12 +1,13 @@
 #pragma once
 
+
 #if defined(SRP_MALLOC) && defined(SRP_FREE) && defined(SRP_REALLOC)
 	// ok
 #elif !defined(SRP_MALLOC) && !defined(SRP_FREE) && !defined(SRP_REALLOC)
-	#include <stdlib.h>
-	#define SRP_MALLOC(s) malloc(s)
-	#define SRP_REALLOC(p, s) realloc(p, s)
-	#define SRP_FREE(p) free(p)
+	#include "alloc.h"
+	#define SRP_MALLOC(s) srpMalloc(s)
+	#define SRP_REALLOC(p, s) srpRealloc(p, s)
+	#define SRP_FREE(p) srpFree(p)
 #else
 	#error "Must define all or none of SRP_MALLOC, SRP_FREE, and SRP_REALLOC"
 #endif
