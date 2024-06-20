8f63d105363118155591476778ae61b2b5bcd9dd
Kirill <kitrofimov39@gmail.com>
2024-06-20 18:10:29 +0500

    Add documentation to each member of `SRPFramebuffer`

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -15,27 +15,25 @@
 /** Holds RBGA8888 color buffer and depth buffer */
 typedef struct SRPFramebuffer
 {
-	size_t width;
-	size_t height;
-	size_t size;
-	uint32_t* color;
-	double* depth;
+	size_t width;     /**< Width */
+	size_t height;    /**< Height */
+	size_t size;      /**< N of pixels (width*height) */
+	uint32_t* color;  /**< Pointer to color buffer */
+	double* depth;    /**< Pointer to depth buffer */
 } SRPFramebuffer;
 
 /** Create a framebuffer
- *  @param[in] width Width of a new framebuffer, in pixels
- *  @param[in] height Height of a new framebuffer, in pixels
+ *  @param[in] width Width of a new framebuffer in pixels
+ *  @param[in] height Height of a new framebuffer in pixels
  *  @return A pointer to the created framebuffer */
 SRPFramebuffer* srpNewFramebuffer(size_t width, size_t height);
 
 /** Free a framebuffer
- *  @param[in] this The pointer to SRPFramebuffer, as returned
- *                    from srpNewFramebuffer() */
+ *  @param[in] this The pointer to SRPFramebuffer, as returned from srpNewFramebuffer() */
 void srpFreeFramebuffer(SRPFramebuffer* this);
 
 /** Clear a framebuffer: fill the color with black and depth with -1
- *  @param[in] this The pointer to SRPFramebuffer, as returned
- *                    from srpNewFramebuffer() */
+ *  @param[in] this The pointer to SRPFramebuffer, as returned from srpNewFramebuffer() */
 void framebufferClear(const SRPFramebuffer* this);
 
 /** @} */  // ingroup Framebuffer
